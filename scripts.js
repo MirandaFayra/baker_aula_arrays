fb52df08574f1cfbd071bce9ead3861e2ab21491
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-09-04 20:18:14 -0300

    Finalizar a base do Script no javascript

---
+++ scripts.js
@@ -0,0 +1,58 @@
+console.log('Salve Baker')
+// Variável espaço na memória em que guardamos os dados
+
+// Funções, trechos de código reutilizável 
+
+// ARRAY = LISTA de variáveis 
+
+// ARRAY inicia-se com o indice Zero 
+
+let arrayExplicacao = [56,39, 'salve', true , 78]
+console.log(arrayExplicacao)
+
+// PUSH => Insere o elemento no final do array   - array.push("")
+
+arrayExplicacao.push("baker")
+console.log(arrayExplicacao)
+
+//POP => Retira o elemento no final do array  - array.pop()
+arrayExplicacao.pop()
+console.log(arrayExplicacao)
+
+// UNSHIFT => Adiciona um elemento no inicio de array - array.unshift(" ")
+arrayExplicacao.push("Salveee")
+console.log(arrayExplicacao)
+
+// SHIFT =>  Remove o primeiro elemento de um array e o retorna - array.shift()
+
+arrayExplicacao.shift()
+console.log(arrayExplicacao)
+
+
+// SPLICE (i,n)=>  Remove ou substitui elementos em um array  - array.splice(ind, qtd, 'elemento')
+
+arrayExplicacao.splice(1,2,'Helooo')
+console.log(arrayExplicacao)
+
+// INCLUDES : Verifica se o elemento está ou não no array, retornando um booleano - 
+arrayExplicacao.includes("baker")
+console.log(arrayExplicacao)
+
+//INDEXOF : Retorna o indice da primeira ocorrencia de um valor do array. Caso não for encontrado retorna -1 
+
+arrayExplicacao.indexOf('39')
+console.log(arrayExplicacao)
+
+// SORT: Usado para ordenar os elementos de um array em ordem alfabéticaou numérica
+
+arrayExplicacao.sort ()
+console.log(arrayExplicacao)
+
+// -------------- EXERCICIO 1 --------
+
+// -------------- EXERCICIO 2 --------
+
+// -------------- EXERCICIO 3 -----------
+
+// -------------- EXERCICIO EXTRA --------
+
