9e75b768cb19f191e189175209c40325acdbbf24
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-09-04 22:09:11 -0300

    Finalizar exercicio 1 a 3

--- scripts.js
+++ scripts.js
@@ -69,7 +69,7 @@ console.log('Salve Baker')
 
 // -------------- EXERCICIO 1 --------
 
-const imprimirArray =()=>{
+/*const imprimirArray =()=>{
     let arrayDeStrings = ['a', 'baker', 'salve' ]
     console.log(arrayDeStrings)
 
@@ -87,11 +87,124 @@ const imprimirArray =()=>{
 
 }
 
-imprimirArray()
+imprimirArray()*/
 
 // -------------- EXERCICIO 2 --------
 
+/*const imprimirArray =()=>{
+    let arrayDeStrings = ['a', 'baker', 'salve' ]
+    console.log(arrayDeStrings)
+    
+    let tamanhoArrayDeString = arrayDeStrings.length
+    console.log(tamanhoArrayDeString )
+    console.log(tamanhoArrayDeString[3] ) //acessar posição 3
+
+    let arrayDeNumbers = [1, 29, 94 ]
+    //console.log(arrayDeNumbers)
+
+    let arrayDeMisto = ['a', 29, true ]
+    //console.log(arrayDeMisto)
+    let verificarString = arrayDeMisto.includes('a')
+    console.log(verificarString)
+
+    let verificarNumber = arrayDeMisto.includes(29)
+    console.log(verificarNumber)
+
+    let verificarBolean  = arrayDeMisto.includes(true)
+    console.log(verificarBolean)
+
+    let arrayDeUmValor = [29]
+    console.log(arrayDeUmValor)
+    console.log(arrayDeUmValor[0]) // imprimir o elemento que está no indice 0 
+    console.log(arrayDeUmValor.length) // imprimir o tamanho do array
+
+    let arraySemValor = [ ]
+    //console.log(arraySemValor)
+
+}
+
+imprimirArray()*/
+
+
+//------------- EXEMPLO EXTRA COM DADOS DA PESSOA USUÁRIA ---------
+
+// 1. Crie uma funcao que solicite para a pessoa usuária, o nome, email, senha (nessa ordem)
+// 2. Imprima a posição do nome
+// 3. Imprima os dados da pessoa usuária com a seguinte frase 'Seja bem-vinde NOME ao nosso sistema! A senha cadastrada é : SENHA. Seu email no nosso banco é EMAIL '
+
+/*const solicitarDados =()=>{
+    let nome = prompt('Digite o seu nome')
+    let email = prompt('Digite o seu email')
+    let senha = prompt('Digite o seu senha')
+    let arrayDeDados = [nome, email, senha] // formatando o array para os dados
+    //let arrayDeDados =[]
+    arrayDeDados.push(nome) // adiciona no fim do array
+    arrayDeDados.push(email)  // adiciona no fim do array pós nome
+    arrayDeDados.push(senha) // adiciona no fim do array pós email
+
+    console.log(arrayDeDados) 
+    console.log(` A posição do nome, tem como indice ${arrayDeDados.indexOf(nome)}. Seja bem-vinde ${arrayDeDados[0]} ao nosso sistema! A senha cadastrada é : ${arrayDeDados[2]}. Seu email no nosso banco é ${arrayDeDados[1]} `) 
+
+}
+
+
+solicitarDados()*/
+
+
 // -------------- EXERCICIO 3 -----------
+
+const imprimirArray =()=>{
+    let arrayDeStrings = ['a', 'baker', 'salve' ]
+    console.log(arrayDeStrings)
+
+    let copiaArrayDeStrings = arrayDeStrings
+    console.log(copiaArrayDeStrings)
+
+    copiaArrayDeStrings.push('bananinha')
+    console.log("Esse é o array de cópia ",copiaArrayDeStrings)
+    console.log("Esse é o array original ",arrayDeStrings)
+
+    // Criar a cópia do array que será manipulado => slice vazio
+    let arrayQueSeraCopiadoAtualizado = arrayDeStrings.slice()
+    //console.log(arrayQueSeraCopiadoAtualizado)
+
+    // Estamos manipulando apenas a cópia do array
+    let arrayRetirado = arrayQueSeraCopiadoAtualizado.slice(0,2 , '')
+    console.log(arrayRetirado)
+
+
+    
+    let tamanhoArrayDeString = arrayDeStrings.length
+    console.log(tamanhoArrayDeString )
+    console.log(tamanhoArrayDeString[3] ) //acessar posição 3
+
+    let arrayDeNumbers = [1, 29, 94 ]
+    //console.log(arrayDeNumbers)
+
+    let arrayDeMisto = ['a', 29, true ]
+    //console.log(arrayDeMisto)
+    let verificarString = arrayDeMisto.includes('a')
+    console.log(verificarString)
+
+    let verificarNumber = arrayDeMisto.includes(29)
+    console.log(verificarNumber)
+
+    let verificarBolean  = arrayDeMisto.includes(true)
+    console.log(verificarBolean)
+
+    let arrayDeUmValor = [29]
+    console.log(arrayDeUmValor)
+    console.log(arrayDeUmValor[0]) // imprimir o elemento que está no indice 0 
+    console.log(arrayDeUmValor.length) // imprimir o tamanho do array
+
+    let arraySemValor = [ ]
+    //console.log(arraySemValor)
+
+}
+
+imprimirArray()
+
+
 
 // -------------- EXERCICIO EXTRA --------
 
